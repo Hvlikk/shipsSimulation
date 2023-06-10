d272074271731bb921f91b04e7a557cce04c6bad
Hvlik <emiloszhalicki@gmail.com>
2023-06-10 22:25:33 +0200

    #9, added Iceberg class

---
+++ untitled/src/Iceberg.java
@@ -0,0 +1,11 @@
+public class Iceberg {
+    protected Integer id;
+    protected Integer posX;
+    protected Integer posY;
+
+    public Iceberg(Integer id, Integer posX, Integer posY){
+        this.id = id;
+        this.posX = posX;
+        this.posY = posY;
+    }
+}
