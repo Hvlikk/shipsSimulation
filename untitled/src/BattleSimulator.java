178aed3c56c94de0c787be9c5341787819813d39
Hvlik <emiloszhalicki@gmail.com>
2023-06-10 20:42:12 +0200

    #12, added ending conditions

--- untitled/src/BattleSimulator.java
+++ untitled/src/BattleSimulator.java
@@ -1,7 +1,6 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 public class BattleSimulator {
-
     private final Integer mapHeight;
     private final Integer mapWidth;
     private char map[][];
@@ -32,29 +31,75 @@ public class BattleSimulator {
             for (int j = 0; j < mapWidth; j++)
                 System.out.print(map[i][j]);
             System.out.println(" ");
-
-
         }
         System.out.println("========");
     }
 
+    private Boolean checkEndingCondition(){
+        Boolean britishShipsRemaining = false;
+        Boolean piratesShipRemaining = false;
+
+        for (Ship ship : ships){
+            if (ship instanceof BritishShip) {
+                britishShipsRemaining = true;
+            }
+            else if (ship instanceof  PirateShip) {
+                piratesShipRemaining = true;
+            }
+
+            if (britishShipsRemaining && piratesShipRemaining) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public void simulateBattle() {
-        for (int k = 0; k < 10; k++) {
+        Boolean battleInProgress = true;
+
+        while(battleInProgress){
+            Integer k = 0;
             System.out.println("================");
             System.out.println("Tura numer: " + k);
             System.out.println("================");
-
             showMap();
 
             for (Ship ship : ships) {
                 ship.shipMovement(map, ships);
                 ship.shipAttack(ships);
             }
-
             removeDestroyedShips(map);
 
+            if(checkEndingCondition()){
+                battleInProgress = false;
+            }
             System.out.println("================");
             System.out.println("KONIEC TURY");
+        }
+
+        displaySimulationResult();
+    }
+
+    public void displaySimulationResult()
+    {
+        int britishShipsRemaining = 0;
+        int pirateShipsRemaining = 0;
+
+        for (Ship ship : ships) {
+            if (ship instanceof BritishShip) {
+                britishShipsRemaining++;
+            } else if (ship instanceof PirateShip) {
+                pirateShipsRemaining++;
+            }
+        }
+        System.out.println("Wynik symulacji:");
+        System.out.println("Statki brytyjskie pozostałe: " + britishShipsRemaining);
+        System.out.println("Statki pirackie pozostałe: " + pirateShipsRemaining);
+
+        if (britishShipsRemaining > pirateShipsRemaining) {
+            System.out.println("Zwycięstwo brytyjczyków!");
+        } else {
+            System.out.println("Zwycięstwo piratów!");
         }
     }
 }
