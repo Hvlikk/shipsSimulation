ad278182e12a62633bd4366c082eafd257f1d2c8
Hvlik <emiloszhalicki@gmail.com>
2023-06-10 02:47:35 +0200

    ####

--- untitled/src/BattleSimulator.java
+++ untitled/src/BattleSimulator.java
@@ -1,35 +1,5 @@
 import java.util.ArrayList;
 public class BattleSimulator {
 
-    private final Integer mapHeight;
-    private final Integer mapWidth;
-    private Integer map[][];
-    private ArrayList<Ship> ships;
 
-
-    public BattleSimulator(Integer mapWidth, Integer mapHeight, ArrayList<Ship> ships, Integer map[][]) {
-        this.mapWidth = mapWidth;
-        this.mapHeight = mapHeight;
-        this.ships = ships;
-        this.map = map;
-    }
-
-
-    public void simulateBattle() {
-        for (int k = 0; k < 6; k++) {
-
-            for (Ship ship : ships)
-                ship.shipMovement(map, ships);
-
-            for (int i = 0; i < mapHeight; i++) {
-                for (int j = 0; j < mapWidth; j++)
-                    System.out.print(map[i][j]);
-                System.out.println(" ");
-
-
-            }
-            System.out.println("========");
-        }
-
-    }
 }
--- untitled/src/BritishShip.java
+++ untitled/src/BritishShip.java
@@ -4,56 +4,6 @@ import java.util.Random;
 
 public class BritishShip extends Ship {
 
-    public BritishShip(Integer id, Integer posX, Integer posY, Integer attack, String direction, Integer hp, Integer movement) {
-        super(id, posX, posY, attack, direction, 2, 1);
-        this.movement = 1;
-        setRandomDirection();
-    }
 
-    private void setRandomDirection() {
-        String[] directions = {"North", "East", "West", "South"};
-        Random random = new Random();
-        int index = random.nextInt(directions.length);
-        direction = directions[index];
-    }
-
-    public List<String> getAvailableDirections() {
-        List<String> availableDirections = new ArrayList<>();
-        availableDirections.add("North");
-        availableDirections.add("South");
-        availableDirections.add("East");
-        availableDirections.add("West");
-        return availableDirections;
-    }
-
-    @Override
-    public void shipMovement(Integer map[][], ArrayList<Ship> ships) {
-
-            Integer newX = getPosX();
-            Integer newY = getPosY();
-            List<String> availableDirections = getAvailableDirections();
-            Random random = new Random();
-
-            switch (direction) {
-                case "North" -> newY += movement;
-                case "South" -> newY -= movement;
-                case "West" -> newX -= movement;
-                case "East" -> newX += movement;
-
-            }
-
-            if (isValidMove(newX, newY, map, ships)) {
-                map[getPosY()][getPosX()] = 0;
-                setPosX(newX);
-                setPosY(newY);
-                map[getPosY()][getPosX()] = 1;
-            } else {
-                availableDirections.remove(direction);
-                if (!availableDirections.isEmpty()) {
-                    direction = availableDirections.get(random.nextInt(availableDirections.size()));
-                }
-                shipMovement(map, ships);
-            }
-        }
 }
 
